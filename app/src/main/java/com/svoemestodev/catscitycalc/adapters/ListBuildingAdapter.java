9db585afd80090442bbcb53a6efe3dae0af09d97
svoemesto <57410666+svoemesto@users.noreply.github.com>
2020-06-18 19:11:45 +0300

    64

--- app/src/main/java/com/svoemestodev/catscitycalc/adapters/ListBuildingAdapter.java
+++ app/src/main/java/com/svoemestodev/catscitycalc/adapters/ListBuildingAdapter.java
@@ -28,7 +28,29 @@ public class ListBuildingAdapter extends ArrayAdapter<Building> {
         }
 
         ImageView select_item_bitmap = convertView.findViewById(R.id.select_item_bitmap);
+        ImageView select_item_icon = convertView.findViewById(R.id.select_item_icon);
         select_item_bitmap.setImageBitmap(item.getBitmap());
+        switch (item.getSlot()) {
+            case 1:
+                select_item_icon.setImageDrawable(getContext().getDrawable(R.drawable.ic_blt_black));
+                break;
+            case 2:
+                select_item_icon.setImageDrawable(getContext().getDrawable(R.drawable.ic_blc_black));
+                break;
+            case 3:
+                select_item_icon.setImageDrawable(getContext().getDrawable(R.drawable.ic_blb_black));
+                break;
+            case 4:
+                select_item_icon.setImageDrawable(getContext().getDrawable(R.drawable.ic_brt_black));
+                break;
+            case 5:
+                select_item_icon.setImageDrawable(getContext().getDrawable(R.drawable.ic_brc_black));
+                break;
+            case 6:
+                select_item_icon.setImageDrawable(getContext().getDrawable(R.drawable.ic_brb_black));
+                break;
+            default:
+        }
 
         return convertView;
 
--- app/src/main/java/com/svoemestodev/catscitycalc/database/DbTeamGame.java
+++ app/src/main/java/com/svoemestodev/catscitycalc/database/DbTeamGame.java
@@ -459,108 +459,45 @@ public class DbTeamGame implements Serializable {
                             this.userNIC = GameActivity.mainDbTeamUser.getUserNIC();
                             this.teamID = GameActivity.mainDbTeamUser.getTeamID();
                         }
-//                        final DbTeamGame dbTeamGame = this;
-//
-//                        GameActivity.fbDb.collection("users").document(userUID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-//                            @Override
-//                            public void onSuccess(DocumentSnapshot documentSnapshot) {
-//
-//                                if (!ccaGame.isGameOver()) {
-////                                    // есть запись о юзере - сохраняем скрин на сервере по пути "users/userUID/last_screenshot"
-////                                    // пережимаем файл в жпег
-////                                    String fileToUploadPath = GameActivity.pathToCATScalcFolder + "/screenshot_to_upload.jpg";
-////                                    Bitmap bitmapSource = BitmapFactory.decodeFile(GameActivity.mainCityCalc.getFileScreenshot().getAbsolutePath());
-////                                    File fileToUpload = new File(fileToUploadPath);
-////                                    try {
-////                                        OutputStream fOut = new FileOutputStream(fileToUpload);
-////                                        bitmapSource.compress(Bitmap.CompressFormat.JPEG, 85, fOut);
-////                                        fOut.flush();
-////                                        fOut.close();
-////
-////                                        //                                    Uri uriFile = Uri.fromFile(GameActivity.mainCityCalc.getFileScreenshot());
-////                                        Uri uriFile = Uri.fromFile(fileToUpload);
-////                                        String storRefGamePathOnServer = "users/" + userUID + "/last_screenshot";
-////                                        StorageReference storRefGame = GameActivity.fbStor.getReference().child(storRefGamePathOnServer);
-////                                        storRefGame.putFile(uriFile).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-////                                            @Override
-////                                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-////                                                // скрин удачно залился на сервер - можно заливать инфу об игре
-////
-////                                                final String teamID = (String)documentSnapshot.get("teamID");
-////                                                final String userNIC = (String)documentSnapshot.get("userNIC");
-////                                                if (teamID != null && !teamID.equals("")) {
-////
-////                                                    dbTeamGame.userNIC = userNIC;
-////                                                    dbTeamGame.teamID = teamID;
-////
-////                                                    DocumentReference doc = GameActivity.fbDb.collection("teams").document(teamID).collection("teamGames").document("teamGame");
-////                                                    doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-////                                                        @Override
-////                                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-////                                                            if (task.isSuccessful()) {
-////                                                                if (task.getResult().exists()) {
-////                                                                    DocumentSnapshot documentSnapshot = task.getResult();
-////                                                                    Map<String, Object> map = dbTeamGame.getMap(userUID, userNIC, teamID);
-////                                                                    if (documentSnapshot.getTimestamp("dateScreenshot").toDate().getTime() < ((Date)map.get("dateScreenshot")).getTime()) {
-////                                                                        GameActivity.fbDb.collection("teams").document(teamID).collection("teamGames").document("teamGame").set(dbTeamGame.getMap(userUID, userNIC, teamID));
-////                                                                    }
-////                                                                } else {
-////                                                                    GameActivity.fbDb.collection("teams").document(teamID).collection("teamGames").document("teamGame").set(dbTeamGame.getMap(userUID, userNIC, teamID));
-////                                                                }
-////                                                            }
-////                                                        }
-////                                                    });
-////
-////                                                }
-////
-////                                            }
-////                                        });
-////
-////
-////                                    } catch (IOException e) {
-////                                        e.printStackTrace();
-////                                    }
-//
-//
-//
-//
-//                                    final String teamID = (String)documentSnapshot.get("teamID");
-//                                    final String userNIC = (String)documentSnapshot.get("userNIC");
-//                                    if (teamID != null && !teamID.equals("")) {
-//
-//                                        dbTeamGame.userNIC = userNIC;
-//                                        dbTeamGame.teamID = teamID;
-//
-//                                        DocumentReference doc = GameActivity.fbDb.collection("teams").document(teamID).collection("teamGames").document("teamGame");
-//                                        doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//                                            @Override
-//                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                                                if (task.isSuccessful()) {
-//                                                    if (task.getResult().exists()) {
-//                                                        DocumentSnapshot documentSnapshot = task.getResult();
-//                                                        Map<String, Object> map = dbTeamGame.getMap(userUID, userNIC, teamID);
-//                                                        if (documentSnapshot.getTimestamp("dateScreenshot").toDate().getTime() < ((Date)map.get("dateScreenshot")).getTime()) {
-//                                                            GameActivity.fbDb.collection("teams").document(teamID).collection("teamGames").document("teamGame").set(dbTeamGame.getMap(userUID, userNIC, teamID));
-//                                                        }
-//                                                    } else {
-//                                                        GameActivity.fbDb.collection("teams").document(teamID).collection("teamGames").document("teamGame").set(dbTeamGame.getMap(userUID, userNIC, teamID));
-//                                                    }
-//                                                }
-//                                            }
-//                                        });
-//
-//                                    }
-//
-//
-//
-//
-//
-//
-//
-//                                }
-//
-//                            }
-//                        });
+                        final DbTeamGame dbTeamGame = this;
+
+                        GameActivity.fbDb.collection("users").document(userUID).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                            @Override
+                            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                                if (!ccaGame.isGameOver()) {
+
+                                    final String teamID = (String)documentSnapshot.get("teamID");
+                                    final String userNIC = (String)documentSnapshot.get("userNIC");
+                                    if (teamID != null && !teamID.equals("")) {
+
+                                        dbTeamGame.userNIC = userNIC;
+                                        dbTeamGame.teamID = teamID;
+
+                                        DocumentReference doc = GameActivity.fbDb.collection("teams").document(teamID).collection("teamGames").document("teamGame");
+                                        doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                if (task.isSuccessful()) {
+                                                    if (task.getResult().exists()) {
+                                                        DocumentSnapshot documentSnapshot = task.getResult();
+                                                        Map<String, Object> map = dbTeamGame.getMap(userUID, userNIC, teamID);
+                                                        if (documentSnapshot.getTimestamp("dateScreenshot").toDate().getTime() < ((Date)map.get("dateScreenshot")).getTime()) {
+                                                            GameActivity.fbDb.collection("teams").document(teamID).collection("teamGames").document("teamGame").set(dbTeamGame.getMap(userUID, userNIC, teamID));
+                                                        }
+                                                    } else {
+                                                        GameActivity.fbDb.collection("teams").document(teamID).collection("teamGames").document("teamGame").set(dbTeamGame.getMap(userUID, userNIC, teamID));
+                                                    }
+                                                }
+                                            }
+                                        });
+
+                                    }
+
+                                }
+
+                            }
+                        });
                     }
                 }
             }
